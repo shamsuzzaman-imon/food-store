e3c35588ae27b3082f5edf35df1a5bc46e33c37a
Shamsuzzaman Imon <imonshamsuzzaman@gmail.com>
2021-06-24 15:46:24 +0600

    slider section done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:".pre-btn",
+    nextArrow:".next-btn",
+    autoplay: true,
+    autoplaySpeed: 1000,
+    })
+});
+
