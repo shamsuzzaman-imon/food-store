210cc390179d3af7596d762c9f5d71648ab888f7
Shamsuzzaman Imon <imonshamsuzzaman@gmail.com>
2021-06-26 20:08:08 +0600

    project design done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.food-slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow:".pre-btn",
-    nextArrow:".next-btn",
-    autoplay: true,
-    autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: ".pre-btn",
+        nextArrow: ".next-btn",
+        autoplay: true,
+        autoplaySpeed: 2000,
     })
 });
 
